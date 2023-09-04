b3337df7e246b33ad96e661b261dd638251f8aae
bahittinn <bahittin84@gmail.com>
2023-09-04 22:52:34 +0300

    changes

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculatorWorkings: UILabel!
+    @IBOutlet weak var calculatorResults: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func allClearTap(_ sender: Any) {
+    }
+    
+    @IBAction func backTapped(_ sender: Any) {
+    }
+    @IBAction func percentTapped(_ sender: Any) {
+    }
+    @IBAction func divideTapped(_ sender: Any) {
+    }
+    @IBAction func timesTapped(_ sender: Any) {
+    }
+    @IBAction func minusTapped(_ sender: Any) {
+    }
+    @IBAction func plusTapped(_ sender: Any) {
+    }
+    
+    
 }
 
